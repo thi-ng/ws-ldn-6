c767e26767a550e3365218c038dea6d38e785e7d
Karsten Schmidt <k@postspectacular.com>
2016-04-07 12:59:20 +0100

    update jogl deps

--- day1/project.clj
+++ day1/project.clj
@@ -4,4 +4,7 @@
   :license {:name "Apache Software License"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thi.ng/geom "0.0.1146-SNAPSHOT"]])
+                 [thi.ng/geom "0.0.1146-SNAPSHOT"]]
+  :profiles {:osx {:dependencies
+                   [[org.jogamp.gluegen/gluegen-rt "2.3.2" :classifier "natives-macosx-universal"]
+                    [org.jogamp.jogl/jogl-all "2.3.2" :classifier "natives-macosx-universal"]]}})
